ecab37ef4526e3ede099931b1c3d81d35d376b32
SvetlozarKalchev <svetlozar@kalchev.eu>
2016-01-09 22:37:06 +0100

    Add unfinished branch method

--- BranchAndBound.h
+++ BranchAndBound.h
@@ -1,27 +1,23 @@
-//
-// Created by Sabrina on 10.11.2015.
-//
-
 #ifndef BRANCHANDBOUND_BRANCHANDBOUND_H
 #define BRANCHANDBOUND_BRANCHANDBOUND_H
-using namespace std;
+
 #include <iostream>
 #include <list>
 
-
+using namespace std;
 
 class BranchAndBound {
 public:
-    struct Problem{
+    struct Problem {
         int l; //lower bound
-        int * assignment; //Assignment, dh welche Person welchen Job macht, wird hier reingeschrieben
+        int *assignment; //Assignment, dh welche Person welchen Job macht, wird hier reingeschrieben
         int count; //zaehlt wie viele Aufgaben schon vergeben wurden
         Problem(int n) : l{0}, assignment{new int[n]{}}, count{0} {}
         ~Problem() {delete[] assignment;}
     };
 
 private:
-    typedef std::list<Problem> BBlist;
+    typedef list<Problem> BBlist;
     BBlist liste; //Liste, die die Probleme enthaelt
     int n; //Anzahl der Jobs bzw. Personen
     int** matrix; //Kostenmatrix
@@ -31,9 +27,11 @@ private:
     Problem *s;
 
 public:
+    /* Constructor and destructor*/
     BranchAndBound() : u{2147483647}, liste{}, s {new Problem(n)}, minimalAssignment{new int[n]{}} {liste.push_front(
                 (const Problem &) s);}
     ~BranchAndBound() {  delete[] matrix;}
+
 
     int* assignment(int n, int **kosten);
     BBlist branch(Problem problem);
@@ -43,71 +41,175 @@ public:
 
 };
 
-//Array: [Zeile][Spalte]
+/*
+    Array: [Zeile][Spalte]
+    Method receives number of jobs/workers and cost matrix.
+*/
 int* BranchAndBound::assignment(int n, int **kosten) {
-    this->n=n;
+    /* Assign number of workers and cost matrix to class variables */
+    this->n = n;
     this->matrix=kosten;
-    while(!liste.empty()){
-        Problem problem = minProblem(); //Problem mit minimaler Lower Bound
-        liste.pop_back(); //Problem mit minimaler Lower Bound wird aus Liste entfernt
-        BBlist teilProblemVector = branch(problem); //Teilt Problem in Teilprobleme
-        for(int i = 0; i < teilProblemVector.size(); i++) { //Fur jedes Teilproblem wird bound-Funktion aufgerufen um L zu berechnen
-            Problem teilproblem = teilProblemVector.front();
-            bound(teilproblem);
-            teilProblemVector.pop_back();
+
+    /*
+        While the list contains problems:
+        1. Go through each problem
+        2. Create branches, e.g subproblems
+        3. Go through each subproblem to calculate lower bound and determine validity
+        4. Return worker/job combination with lowest cost
+    */
+    while(!liste.empty())
+    {
+        /* Get the problem with the minimal lower bound. Then remove it from the global problem list. */
+        Problem problem = minProblem();
+        liste.pop_back();
+
+        /* Get a list containing all permutations of the problem. */
+        BBlist sub_problem_list = branch(problem);
+
+        /* Calculate lower bound for each subproblem */
+        for(int i = 0; i < sub_problem_list.size(); i++)
+        {
+            Problem sub_problem = sub_problem_list.front();
+            bound(sub_problem);
+            sub_problem_list.pop_back();
         }
     }
+
     return minimalAssignment;
 }
+/*
+    Method that accepts a problem and creates a list with all possible variations.
+*/
+BranchAndBound::BBlist BranchAndBound::branch(Problem problem)
+{
+    /*
+        1. Find out how many jobs need to be allocated
+        2. Start from the first empty worker/job position
+        3. Find lowest value for each job
+        4. Put the problem variation in the list
+        5. At the end, return the branch list.
+    */
+    BranchAndBound::BBlist problem_list;
+    int number_of_assignments = problem.count;
+    int workers = this->n;
+    int **matrix = this->matrix;
+    int job;
 
-BranchAndBound::BBlist BranchAndBound::branch(Problem problem) {
+    /* i = collumn */
+    for(int i = number_of_assignments; i < workers; i++)
+    {
+        /* Create a new problem object. Its assignment field is updated and the object is later put in the branch list. */
+        BranchAndBound::Problem sub_problem(this->n);
 
+        /* Go over each row and get the lowest unassigned value. */
+        job = i;
+
+        for(int row = 0; row < workers; row++)
+        {
+            /* TODO:
+                1. Add method to check if the current worker is already given a job.
+                2. Build a new branch from the possible values
+            */
+        }
+
+        problem_list.push_front((const Problem &)sub_problem);
+    }
+
+    return problem_list;
 }
 
-void BranchAndBound::bound(Problem teilproblem) {
-    //lower bound berechnen
+/*
+    Method that accepts a problem and calculates the sum of all costs.
+*/
+void BranchAndBound::bound(Problem sub_problem)
+{
     int lowerBound = 0;
-    int * currentAssignment = new int[n] {};
-    for(int i = 0; i < teilproblem.count; i++) { //bereits festgelegte Agenten
-        lowerBound += matrix[teilproblem.assignment[i]][i];   //teilproblem.assignment[i]*(i+1)]; //teilproblem.assignment[i-1]
-        currentAssignment[i] = teilproblem.assignment[i];
+    int *currentAssignment = new int[n] {};
+    /*
+        Go through the cost matrix and sum the corresponding worker/job cost values
+        The problem struct contains an array, denoting which job is given to which worker
+        Example: [1][0][2] means J0 is given to W1, J1 to W0, and J2 to W2
+
+        The count variable shows how many workers are given jobs. For those problems where
+        not all workers are given a job, the minimum job cost value is added to the LB for each
+        unassigned job.
+    */
+    int worker;
+    int allocations = sub_problem.count;
+    /*
+        For each assigned job, add the cost to the LB.
+    */
+    for(int i = 0; i < allocations; i++) {
+        worker = sub_problem.assignment[i];
+
+        lowerBound += matrix[worker][i];
+
+        currentAssignment[i] = sub_problem.assignment[i];
     }
-    for(int j = teilproblem.count; j < n; j++) { //noch nicht festgelegt --> minimum
+
+    /*
+        For each unassigned job, find the minumum job cost and add it to the LB.
+        1. Allocate new array with as many slots as workers/jobs.
+        2. Take cost value on position j from each row of the cost matrix.
+    */
+    for(int j = allocations; j < n; j++)
+    {
         int *array = new int[n];
+        /*
+            Starting from the first unassigned job, store all its cost value for each worker in an array.
+            Then compare ???
+        */
         for(int i = 0; i < n; i++) { //i = Spalte
             array[i] = matrix[i][j];
-            for(int x = 0; x < teilproblem.count; x++) if(currentAssignment[x]==i) array[i]=2147483647;
+
+            for(int x = 0; x < allocations; x++)
+            {
+                if(currentAssignment[x] == i)
+                {
+                    array[i]=2147483647;
+                }
+            }
         }
 
         // Spalte j durchlaufen und Minimum suchen
+        /*
+
+        */
         int agentMin = 0;
-        for(int k = 1; k<n; k++) {
+        for(int k = 1; k<n; k++)
+        {
             if(array[k]<array[agentMin]) agentMin=k;
         }
         lowerBound +=array[agentMin];
         currentAssignment[j]=agentMin;
     }
 
-    //pruefen ob currentAssignment gueltige Loesung ist
-    bool gueltigeLoesung = true;
-    for(int l=0; l<n-1; l++) {
-        for(int m=l+1; m<n; m++) {
-            if(currentAssignment[l] == currentAssignment[m]) {
-                gueltigeLoesung = false;
+    /*
+        Check if the current job assignment is a valid solution. Condition for that is each
+        job must be given to a different worker.
+    */
+    bool is_valid_solution = true;
+    for(int l = 0; l < n-1; l++)
+    {
+        for(int m = l+1; m < n; m++)
+        {
+            if(currentAssignment[l] == currentAssignment[m])
+            {
+                is_valid_solution = false;
                 break;
             }
         }
     }
 
     //gueltige Loesung
-    if(lowerBound < u && gueltigeLoesung) {
+    if(lowerBound < u && is_valid_solution) {
         this->u = lowerBound;
         minimalAssignment = currentAssignment;
     }
 
     //keine gueltige Loesung
     else {
-        liste.push_front(teilproblem);
+        liste.push_front(sub_problem);
     }
 }
 
