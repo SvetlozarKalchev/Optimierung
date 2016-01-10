8f2c4247a3ca317c08a71413f507ad8ddcad42e2
sjutz <s.jutz@live.de>
2016-01-10 20:30:03 +0100

    More documentation

--- BranchAndBound.h
+++ BranchAndBound.h
@@ -7,48 +7,45 @@
 using namespace std;
 
 class BranchAndBound {
-public:
+private:
     struct Problem {
         int l; //lower bound
-        int *assignment; //Assignment, dh welche Person welchen Job macht, wird hier reingeschrieben
-        int count; //zaehlt wie viele Aufgaben schon vergeben wurden
+        int *assignment; //assigns tasks to agents
+        int count; //counts how many tasks are already assigned
+
+        /* Contstructor and destructor */
         Problem(int n) : l{0}, assignment{new int[n]{}}, count{0} {}
         ~Problem() {delete[] assignment;}
-        Problem(const Problem& p) : l{p.l}, count{p.count}, assignment{new int[sizeof(p.assignment)]} { for(int i = 0; i < sizeof(p.assignment); i++) {
+        Problem(const Problem& p) : l{p.l}, count{p.count}, assignment{new int[sizeof(p.assignment)]} {
+            for(int i = 0; i < sizeof(p.assignment); i++) {
                 assignment[i] = p.assignment[i];
             }; }
+
+        /* = operator */
         Problem& operator=(const Problem& p) {
             int *newAssignment = new int[sizeof(p.assignment)];
             for(int i = 0; i < sizeof(p.assignment); i++) {
                 newAssignment[i] = p.assignment[i];
             }
-
-            //std::copy(assignment, assignment + sizeof(p.assignment), p.assignment);
             delete[] assignment;
             assignment = newAssignment;
             l = p.l;
             count = p.count;
             return *this;
         }
-
-
     };
 
-private:
     typedef list<Problem> BBlist;
-    BBlist liste; //Liste, die die Probleme enthaelt
-    int n; //Anzahl der Jobs bzw. Personen
-    int** matrix; //Kostenmatrix
-    int u; // Max range of int instead of infinity
-    //Problem * min; //zeigt auf Problem mit kleinster Lower bound
+    BBlist liste; //list containing problems that need to be processed
+    int n; //number of tasks, number of agents
+    int** matrix; //cost matrix
+    int u; // upper bound, max range of int instead of infinity
     int * minimalAssignment;
-    //Problem *s;
 
 public:
     /* Constructor and destructor*/
     BranchAndBound() : u{2147483647}, liste{} {}
     ~BranchAndBound() {  delete[] matrix;}
-
 
     int* assignment(int n, int **kosten);
     BBlist branch(Problem problem);
@@ -60,8 +57,8 @@ public:
 };
 
 /*
-    Array: [Zeile][Spalte]
-    Method receives number of jobs/workers and cost matrix.
+    Array: [row][column]
+    Method receives number of jobs/workers and cost matrix. Returns an int-array with the assignment so that the costs are minimal.
 */
 int* BranchAndBound::assignment(int n, int **kosten) {
     /* Assign number of workers and cost matrix to class variables */
@@ -133,6 +130,10 @@ BranchAndBound::BBlist BranchAndBound::branch(Problem problem)
     return problem_list;
 }
 
+
+/*
+    Method that accepts an assignment and checks wheter the assignemnt is valid or not.
+ */
 bool BranchAndBound::is_solution_valid(int *current_assignment)
 {
     bool is_valid = true;
@@ -154,14 +155,14 @@ bool BranchAndBound::is_solution_valid(int *current_assignment)
     return is_valid;
 }
 /*
-    Method that accepts a problem and calculates the sum of all costs.
+    Method that accepts a problem and calculates its lower bound.
 */
 void BranchAndBound::bound(Problem sub_problem)
 {
     int lowerBound = 0;
     int *currentAssignment = new int[n] {};
     /*
-        Go through the cost matrix and sum the corresponding worker/job cost values
+        Go through the cost matrix and sum up the corresponding worker/job cost values
         The problem struct contains an array, denoting which job is given to which worker
         Example: [1][0][2] means J0 is given to W1, J1 to W0, and J2 to W2
 
@@ -185,16 +186,12 @@ void BranchAndBound::bound(Problem sub_problem)
     /*
         For each unassigned job, find the minumum job cost and add it to the LB.
         1. Allocate new array with as many slots as workers/jobs.
-        2. Take cost value on position j from each row of the cost matrix.
+        2. Take cost value on position j from each row of the cost matrix. If agent is already assigned, set value to 2147483647 (maximal int value, this agent can only do one task)
     */
     for(int j = allocations; j < n; j++)
     {
         int *array = new int[n];
-        /*
-            Starting from the first unassigned job, store all its cost value for each worker in an array.
-            Then compare ???
-        */
-        for(int i = 0; i < n; i++) { //i = Spalte
+        for(int i = 0; i < n; i++) {
             array[i] = matrix[i][j];
 
             for(int x = 0; x < allocations; x++)
@@ -206,9 +203,8 @@ void BranchAndBound::bound(Problem sub_problem)
             }
         }
 
-        // Spalte j durchlaufen und Minimum suchen
         /*
-
+            Search array for minimal value and assign agent with minimal value to this task.
         */
         int agentMin = 0;
         for(int k = 1; k<n; k++)
@@ -225,19 +221,24 @@ void BranchAndBound::bound(Problem sub_problem)
     */
     bool valid_solution = is_solution_valid(currentAssignment);
 
-    //gueltige Loesung
+    /*
+        Lowerbound is smaller than the upper bound and solution is valid: Change upper bound to lower bound, change minimalAssignment to currentAssignment
+     */
     if(lowerBound < u && valid_solution) {
         this->u = lowerBound;
         minimalAssignment = currentAssignment;
     }
-
-        //keine gueltige Loesung
-    else {
+        /*
+            Solution is not valid: Add Problem to list.
+        * */
+    else if(!valid_solution) {
         liste.push_front(sub_problem);
     }
 }
 
-
+/*
+    Method finds Problem with minimal lowerbound in the list of problems.
+ * */
 BranchAndBound::Problem BranchAndBound::minProblem() {
     //liste->sort();
     return liste.front();
